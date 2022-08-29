b7b2ab052b9b583d07be792b46bea9c2e76dddc7
Kenneth Ascheri <kascheri12@gmail.com>
2022-08-28 22:55:30 -0700

    Add recursive function to KenBox, format page, implement KenBox in app

--- KenBox.py
+++ KenBox.py
@@ -7,5 +7,21 @@ class KenBox:
     def __init__(self,box=None):
         self.containedBox = box
     
-    def getNumberOfChildBoxes():
+    def getChildBoxRecurse(self,box):
+        if (box.containedBox is not None):
+            return self.getChildBoxRecurse(box.containedBox) + 1
         return 0
+    
+    def getNumberOfChildBoxesRecursive(self):
+        return self.getChildBoxRecurse(self)
+    
+    def getNumberOfChildBoxesIterative(self):
+        i = 0
+        child = self.containedBox
+        while (child is not None):
+            i += 1
+            if (child.containedBox is not None):
+                child = child.containedBox
+            else:
+                child = None
+        return i
--- app.py
+++ app.py
@@ -3,16 +3,41 @@
 #
 from KenBox import KenBox
 from flask import Flask, render_template, request
+import time
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
     children = None
-    if request.method == 'POST':
-        children = request.form.get('children')
-        parent = KenBox()
-    return render_template('index.html',results=children)
+    
+    myKenBox = KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox(KenBox())))))))))))))))))))))))))))))))))))))))))
+    start_recursive = time.time()
+    children_recur = myKenBox.getNumberOfChildBoxesRecursive()
+    end_recursive = time.time()
+    
+    start_iterative = time.time()
+    children_iter = myKenBox.getNumberOfChildBoxesIterative()
+    end_iterative = time.time()
+    
+    time_recur = (end_recursive - start_recursive)
+    time_iter = (end_iterative - start_iterative)
+    perc_faster = (time_recur - time_iter) / time_iter * 100
+    
+    results = {
+        'children_recur': children_recur,
+        'time_recur': end_recursive - start_recursive,
+        'children_iter': children_iter,
+        'time_iter': end_iterative - start_iterative,
+        'perc_faster': perc_faster
+    }
+    return render_template('index.html'
+                ,children_recur=children_recur
+                ,time_recur=time_recur
+                ,children_iter=children_iter
+                ,time_iter=time_iter
+                ,perc_faster=perc_faster
+                ,results=results)
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
